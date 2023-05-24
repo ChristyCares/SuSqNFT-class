21bf9fb1a84a416b7cf9ee26d15bfed7d758c1cc
ChristyCares <cettefleur@protonmail.ch>
2023-05-24 15:01:18 -0400

    Create HelloWorld.sol
    Foundational syntax:
    * define license use
    * declare pragma for compiler
    * create first contract
    
    // This is the basic syntax. Will add more later.

---
+++ HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.1;
+
+contract FirstContract {
+   uint var1;
+
+   function set(uint x) public {
+       var1 = x;
+   }
+
+   function get() public view returns (uint) {
+       return var1;
+   }
+}
